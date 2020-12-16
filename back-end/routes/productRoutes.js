c7dea56766ab16fedc1accae28f0b738b39d9bd9
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-16 14:29:13 +0800

    Add redux state into HomeScreen

--- back-end/routes/productRoutes.js
+++ back-end/routes/productRoutes.js
@@ -11,8 +11,8 @@ const router = express.Router();
 router.get(
 	"/",
 	asyncHandler(async (req, res) => {
-		const products = await Product.find({});
-
+    const products = await Product.find({});
+    
 		res.json(products);
 	})
 );
