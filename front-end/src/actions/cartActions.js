b9803064dbc517e1b70a95c902501a40400d7ed8
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-17 10:58:18 +0800

    Add Paymenyt Method and save payment method

--- front-end/src/actions/cartActions.js
+++ front-end/src/actions/cartActions.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
+import {
+	CART_ADD_ITEM,
+	CART_REMOVE_ITEM,
+	CART_SAVE_SHIPPING_ADDRESS,
+	CART_SAVE_PAYMENT_METHOD,
+} from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
 	const { data } = await axios.get(`/api/products/${id}`);
@@ -30,4 +35,10 @@ export const saveShippingAddress = (formData) => (dispatch) => {
 	dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: formData });
 
 	localStorage.setItem("shippingAddress", JSON.stringify(formData));
+};
+
+export const savePaymentMethod = (formData) => (dispatch) => {
+	dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: formData });
+
+	localStorage.setItem("paymentMethod", JSON.stringify(formData));
 };
