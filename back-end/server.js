d3252c1901566d18fe1caffd24a81f7474ca07e4
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-19 16:18:31 +0800

    Ready to deploy

--- back-end/server.js
+++ back-end/server.js
@@ -2,7 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
-import morgan from 'morgan';
+import morgan from "morgan";
 
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -18,8 +18,8 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
 }
 
 app.use(express.json());
@@ -35,6 +35,14 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/front-end/build")));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"))
+	);
+}
 
 app.use(notFound);
 
