887df7cde09baa606997056cf19e1c5c5cd6d086
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-19 11:19:57 +0800

    Add mark as delivered functionality for admin user

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -60,14 +60,14 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 	const order = await Order.findById(req.params.id);
 
 	if (order) {
-		(order.isPaid = true),
-			(order.paidAt = Date.now()),
-			(order.paymentResult = {
-				id: req.body.id,
-				status: req.body.status,
-				update_time: req.body.update_time,
-				email_address: req.body.payer.email_address,
-			});
+		order.isPaid = true;
+		order.paidAt = Date.now();
+		order.paymentResult = {
+			id: req.body.id,
+			status: req.body.status,
+			update_time: req.body.update_time,
+			email_address: req.body.payer.email_address,
+		};
 
 		const updatedOrder = await order.save();
 
@@ -99,10 +99,32 @@ const getOrders = asyncHandler(async (req, res) => {
 	res.json(orders);
 });
 
+// @desc    Update order to delivered
+// @route   PUT api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id);
+
+	if (order) {
+		order.isDelivered = true;
+		order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+    
+    console.log(order)
+	} else {
+		res.status(404);
+		throw new Error("Order not found");
+	}
+});
+
 export {
 	addOrderItems,
 	getOrderById,
 	updateOrderToPaid,
 	getMyOrders,
 	getOrders,
+	updateOrderToDelivered,
 };
